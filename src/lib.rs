8700b2f0de85eba124ae07a1276f2196aecd2011
mfirhas <fathir.irhas@gmail.com>
2026-05-22 14:16:37 +0700

    Separate equality to module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 mod conversion;
+mod equality;
 mod error;
 mod macros;
 mod mutation;
@@ -149,46 +150,6 @@ impl<V: Validator, const MIN: usize, const MAX: usize, const ASCII_ONLY: bool>
     #[inline]
     pub const fn capacity(&self) -> usize {
         MAX
-    }
-}
-
-impl<LHSV: Validator, RHSV: Validator, const MIN: usize, const MAX: usize, const ASCII_ONLY: bool>
-    PartialEq<GString<RHSV, MIN, MAX, ASCII_ONLY>> for GString<LHSV, MIN, MAX, ASCII_ONLY>
-{
-    fn eq(&self, other: &GString<RHSV, MIN, MAX, ASCII_ONLY>) -> bool {
-        self.len == other.len && self.buf[..self.len] == other.buf[..other.len]
-    }
-}
-
-impl<V: Validator, const MIN: usize, const MAX: usize, const ASCII_ONLY: bool> PartialEq<str>
-    for GString<V, MIN, MAX, ASCII_ONLY>
-{
-    fn eq(&self, other: &str) -> bool {
-        self.as_str() == other
-    }
-}
-
-impl<V: Validator, const MIN: usize, const MAX: usize, const ASCII_ONLY: bool> PartialEq<&str>
-    for GString<V, MIN, MAX, ASCII_ONLY>
-{
-    fn eq(&self, other: &&str) -> bool {
-        self.as_str() == *other
-    }
-}
-
-impl<V: Validator, const MIN: usize, const MAX: usize, const ASCII_ONLY: bool>
-    PartialEq<GString<V, MIN, MAX, ASCII_ONLY>> for str
-{
-    fn eq(&self, other: &GString<V, MIN, MAX, ASCII_ONLY>) -> bool {
-        self == other.as_str()
-    }
-}
-
-impl<V: Validator, const MIN: usize, const MAX: usize, const ASCII_ONLY: bool>
-    PartialEq<GString<V, MIN, MAX, ASCII_ONLY>> for &str
-{
-    fn eq(&self, other: &GString<V, MIN, MAX, ASCII_ONLY>) -> bool {
-        *self == other.as_str()
     }
 }
 

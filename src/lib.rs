41c37efac1e5b937ed2de0783e4d69458fdfc2da
mfirhas <fathir.irhas@gmail.com>
2026-05-26 23:37:51 +0700

    Change self validate to reference

--- src/lib.rs
+++ src/lib.rs
@@ -139,9 +139,9 @@ impl<V: Validator, const MIN: usize, const MAX: usize, const ASCII_ONLY: bool>
     }
 
     #[inline(always)]
-    pub fn validate(self) -> Result<Self, GStringError<V::Err>> {
-        V::validate(self).map_err(GStringError::Validation)?;
-        Ok(self)
+    pub fn validate(&self) -> Result<Self, GStringError<V::Err>> {
+        V::validate(&self).map_err(GStringError::Validation)?;
+        Ok(*self)
     }
 
     #[inline]
